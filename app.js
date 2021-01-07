bbcb9989babf0a66829316d823cb95c75bff2c2d
kellenrowe <kellenrowe@gmail.com>
2021-01-06 16:01:43 -0800

    started route handling and setup

---
+++ app.js
@@ -0,0 +1,50 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const itemsRoutes = require("./routes/itemsRoutes");
+const middleware = require("./middleware");
+const morgan = require("morgan");
+
+app.use(morgan('dev'));
+app.use(express.json());
+
+// process traditional form data => req.body
+app.use(express.urlencoded({ extended: true }));
+
+// useful error class to throw
+const { NotFoundError, BadRequestError } = require("./expressError");
+
+app.use("/items", itemsRoutes);
+
+/** Finds mean of nums in qs: returns {operation: "mean", result } */
+app.get("/mean", function (req, res) {
+  
+});
+
+/** 404 handler: matches unmatched routes; raises NotFoundError. */
+app.use(function (req, res, next) {
+  return next(new NotFoundError());
+});
+
+/** Error handler: logs stacktrace and returns JSON error message. */
+app.use(function (err, req, res, next) {
+  const status = err.status || 500;
+  const message = err.message;
+  if (process.env.NODE_ENV !== "test") console.error(status, err.stack);
+  return res.status(status).json({ error: { message, status } });
+});
+
+
+
+module.exports = app;
+
+
+
+
+/** 
+ * [
+ *  {name: "name", price: "price"},
+ *  {name: "name", price: "price"}
+ * ]
+ */
